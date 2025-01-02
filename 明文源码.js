b0d53e3e2ae9ff3af9f6de160c8d27209d8ea774
gusbobo <ll2648918569@gmail.com>
2025-01-02 09:14:48 +0800

    Update 明文源码.js

--- 明文源码.js
+++ 明文源码.js
@@ -1,7 +1,7 @@
 
 import { connect } from 'cloudflare:sockets';
 
-let userID = '';
+let userID = '002ed6c9-a992-4b2e-adc4-f44746ff9447';
 let proxyIP = '';
 let sub = '';
 let subConverter = 'SUBAPI.fxxk.dedyn.io';
